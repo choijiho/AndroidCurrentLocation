8aefd9f098bedfcbfcc612702faa01169f404b83
Evangelist <Evangelist@192.168.1.5>
2013-05-23 00:07:32 +0900

    init

---
+++ CurrentLocation/src/net/gringrid/currentlocation/CurrentLocationMain.java
@@ -0,0 +1,22 @@
+package net.gringrid.currentlocation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CurrentLocationMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_current_location_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.current_location_main, menu);
+		return true;
+	}
+
+}
